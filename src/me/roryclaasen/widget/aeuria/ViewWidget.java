0e296e0675597169f469ec1741336988a35caf4b
GOGO98901 <gogo98901@live.co.uk>
2016-08-14 14:53:35 +0100

    working on widget but I'm finding it harder than first thought

--- src/me/roryclaasen/widget/aeuria/ViewWidget.java
+++ src/me/roryclaasen/widget/aeuria/ViewWidget.java
@@ -1,9 +1,6 @@
 package me.roryclaasen.widget.aeuria;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -12,11 +9,11 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.widget.RemoteViews;
+import me.roryclaasen.widget.aeuria.util.FancyTime;
 
 public class ViewWidget extends AppWidgetProvider {
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-
 		Timer timer = new Timer();
 		timer.scheduleAtFixedRate(new MyTime(context, appWidgetManager), 1, 1000);
 	}
@@ -25,17 +22,19 @@ public class ViewWidget extends AppWidgetProvider {
 		RemoteViews remoteViews;
 		AppWidgetManager appWidgetManager;
 		ComponentName thisWidget;
-		DateFormat format = SimpleDateFormat.getTimeInstance(SimpleDateFormat.MEDIUM, Locale.getDefault());
+		FancyTime time;
 
 		public MyTime(Context context, AppWidgetManager appWidgetManager) {
 			this.appWidgetManager = appWidgetManager;
 			remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
 			thisWidget = new ComponentName(context, ViewWidget.class);
+			time = new FancyTime(context, new Date());
 		}
 
 		@Override
 		public void run() {
-			remoteViews.setTextViewText(R.id.widget_textview, "TIME = " + format.format(new Date()));
+			remoteViews.setTextViewText(R.id.widget_hour, time.getHour());
+			remoteViews.setTextViewText(R.id.widget_minute, time.getMinute());
 			appWidgetManager.updateAppWidget(thisWidget, remoteViews);
 		}
 	}
---
+++ src/me/roryclaasen/widget/aeuria/util/FancyTime.java
@@ -0,0 +1,128 @@
+package me.roryclaasen.widget.aeuria.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import android.content.Context;
+import me.roryclaasen.widget.aeuria.R;
+
+public class FancyTime {
+	private final Context _context;
+	private final DateFormat format = new SimpleDateFormat("hh:mm", Locale.getDefault());
+
+	private String time;
+
+	public FancyTime(Context context, Date date) {
+		this._context = context;
+		time = format.format(date);
+	}
+
+	public String getHour() {
+		String hour = time.split(":")[0];
+		try {
+			int number = Integer.parseInt(hour);
+			return getWord(number);
+		} catch (Exception e) {
+			return _context.getString(R.string.error);
+		}
+	}
+
+	public String getMinute() {
+		String minute = time.split(":")[1];
+		try {
+			int number = Integer.parseInt(minute);
+			if (number == 10 || number == 20 || number == 30 || number == 40 || number == 50) return getWord(number);
+
+			if (number < 10) return _context.getString(R.string.ohh) + "-" + getWord(number);
+			if (number < 20) return getWord(number);
+			if (number < 30) return getWord(20) + "-" + getWord(number - 20);
+			if (number < 40) return getWord(30) + "-" + getWord(number - 30);
+			if (number < 50) return getWord(40) + "-" + getWord(number - 40);
+			return getWord(50) + "-" + getWord(number - 50);
+		} catch (Exception e) {
+			return _context.getString(R.string.error);
+		}
+	}
+
+	private String getWord(int number) {
+		switch (number) {
+			case (0): {
+				return _context.getString(R.string.zero);
+			}
+			case (1): {
+				return _context.getString(R.string.one);
+			}
+			case (2): {
+				return _context.getString(R.string.two);
+			}
+			case (3): {
+				return _context.getString(R.string.three);
+			}
+			case (4): {
+				return _context.getString(R.string.four);
+			}
+			case (5): {
+				return _context.getString(R.string.five);
+			}
+			case (6): {
+				return _context.getString(R.string.six);
+			}
+			case (7): {
+				return _context.getString(R.string.seven);
+			}
+			case (8): {
+				return _context.getString(R.string.eight);
+			}
+			case (9): {
+				return _context.getString(R.string.nine);
+			}
+			case (10): {
+				return _context.getString(R.string.ten);
+			}
+			case (11): {
+				return _context.getString(R.string.eleven);
+			}
+			case (12): {
+				return _context.getString(R.string.twelve);
+			}
+			case (13): {
+				return _context.getString(R.string.thirteen);
+			}
+			case (14): {
+				return _context.getString(R.string.fourteen);
+			}
+			case (15): {
+				return _context.getString(R.string.fithteen);
+			}
+			case (16): {
+				return _context.getString(R.string.sixteen);
+			}
+			case (17): {
+				return _context.getString(R.string.seventeen);
+			}
+			case (18): {
+				return _context.getString(R.string.eighteen);
+			}
+			case (19): {
+				return _context.getString(R.string.nineteen);
+			}
+			case (20): {
+				return _context.getString(R.string.twenty);
+			}
+			case (30): {
+				return _context.getString(R.string.thirty);
+			}
+			case (40): {
+				return _context.getString(R.string.forty);
+			}
+			case (50): {
+				return _context.getString(R.string.fifty);
+			}
+			default: {
+				return _context.getString(R.string.error);
+			}
+		}
+	}
+}
